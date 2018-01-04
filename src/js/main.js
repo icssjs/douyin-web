3607f4b9f1f49f202a61f79e604b253df6e7969a
绣财 <icssjs@gmail.com>
2018-01-04 11:17:48 +0800

    增加一些样式loader 🙁

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,11 @@
 import React from "react";
 import reactDom from "react-dom";
-import { Button } from "antd"
+import { Button, Input } from "antd"
 
 reactDom.render(
-    <div><Button>提交</Button></div>,
+    <div>
+        <Button>提交</Button>
+        <Input />
+    </div>,
     document.getElementById('app')
 )
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 
 const webpack = require('webpack');
-
+const extractText = require('extract-text-webpack-plugin');
 const option = {
     entry: {
         vendor: ['react', 'antd'],
@@ -13,17 +13,73 @@ const option = {
     },
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            {
+                test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    minetype: 'application/font-woff',
+                },
+            },
+            {
+                test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    minetype: 'application/font-woff',
+                },
+            },
+            {
+                test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    minetype: 'application/octet-stream',
+                },
+            },
+            {
+                test: /\.(eot)(\?v=\d+\.\d+\.\d+)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 10000,
+                    minetype: 'application/vnd.ms-fontobject',
+                },
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?\S*)?$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[name].[ext]?[hash]',
+                    limit: 2048,
+                    outputPath: 'images/'
+                }
+
+            },
+            {
+                test: /\.less$/,
+                use: extractText.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "less-loader"
+                    }],
+                    // use style-loader in development
+                    fallback: "style-loader"
+                })
+            }
         ]
     },
     devServer: {
-        hot: true,
-        inline: true
+        historyApiFallback: true,
+        noInfo: true,
+        hot: false,
     },
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             names: ['vendor']
         }),
+        new extractText('css/style.css')
 
     ]
 }
